cf5ab44f255216ea03224b19f1f169dd922f0af7
Dharanish A M <dharanish816@gmail.com>
2025-01-11 12:53:09 +0530

    Updated Transitions

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,20 @@
-import "../styles/about.css"
+import "../styles/about.css";
 
 export default function About() {
   return (
-    <div className='about-container'>
-    <div className="title">
+    <div className="about-container">
+      <div className="title">
         <div className="title-one">INTRODUCTION</div>
         <div className="title-two">About.</div>
+      </div>
+      <div className="about-content">
+        <div className="left-container-about">
+          <div className="radial-about"></div>
+          <div className="about-image-container"></div>
+          <div className="about-image"></div>
+        </div>
+        <div className="right-container-about"></div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
